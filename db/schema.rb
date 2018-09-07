da32dbb63aa1a08b8b4734a32aa3722e519e02dc
conradskittles <conradszklarz@gmail.com>
2018-09-06 22:09:47 -0500

    updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180821194741) do
+ActiveRecord::Schema.define(version: 20180905220649) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,10 @@ ActiveRecord::Schema.define(version: 20180821194741) do
     t.datetime "updated_at", null: false
     t.text "description"
     t.date "released_on"
+    t.string "cast"
+    t.string "director"
+    t.string "duration"
+    t.string "image_file_name", default: ""
   end
 
 end
